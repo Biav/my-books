bec931c2bd925069eaaad06e9259cf99233a7ae2
Biav <beatrizvieirasilva.v@gmail.com>
2019-03-31 21:46:44 -0300

    Project Initial Interface

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <app-header></app-header>
+    <app-books></app-books>
+    <app-footer></app-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Books from './components/Books.vue'
 
 export default {
-  name: 'app',
+  data () {
+    return {
+      //
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-books': Books
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-toolbar
+      color="cyan"
+      dark
+      tabs
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="model"
+          centered
+          color="cyan"
+          slider-color="yellow"
+        >
+          <v-tab
+            v-for="i in 3"
+            :key="i"
+            :href="`#tab-${i}`"
+          >
+            Item {{ i }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+
+    <v-tabs-items v-model="model">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :value="`tab-${i}`"
+      >
+        <v-card flat>
+          <v-card-text v-text="text"></v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
