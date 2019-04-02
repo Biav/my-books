4604e6c1fb83e5af46a59542b19c2ccec99f012e
Biav <beatrizvieirasilva.v@gmail.com>
2019-04-01 23:12:56 -0300

    Navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,33 +20,18 @@
       </v-btn>
 
       <template v-slot:extension>
+        
         <v-tabs
           v-model="model"
           centered
           color="cyan"
           slider-color="yellow"
-        >
-          <v-tab
-            v-for="i in 3"
-            :key="i"
-            :href="`#tab-${i}`"
-          >
-            Item {{ i }}
-          </v-tab>
+        > 
+          <v-tab to="/books">Books</v-tab>
+          <v-tab to="/setting">Settings</v-tab>
+          
         </v-tabs>
       </template>
     </v-toolbar>
-
-    <v-tabs-items v-model="model">
-      <v-tab-item
-        v-for="i in 3"
-        :key="i"
-        :value="`tab-${i}`"
-      >
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
   </div>
 </template>
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Setting from './components/Setting';
+import Books from './components/Books';
+
+export const routes = [
+    {path:'/books', component: Books},
+    {path:'/setting', component: Setting}
+];
+
